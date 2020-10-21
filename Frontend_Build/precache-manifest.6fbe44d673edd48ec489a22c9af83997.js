7c4f73e407bf3e945da0d0bde7611217b1f42fc9
Tarun Kamboj <kambojtarun02@gmail.com>
2020-10-21 20:11:56 +0530

    digit classifier

---
+++ Frontend_Build/precache-manifest.6fbe44d673edd48ec489a22c9af83997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffeaabdf969cd7acfe92c87ab270f4eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a71431dbfb7b20febd7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69ad9491f2ef52970c57",
+    "url": "/static/js/2.7732fc1b.chunk.js"
+  },
+  {
+    "revision": "14ad4febf86d4a8af57c34cca7998e15",
+    "url": "/static/js/2.7732fc1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a71431dbfb7b20febd7",
+    "url": "/static/js/main.1340c9d1.chunk.js"
+  },
+  {
+    "revision": "70926a7e69e56a32e2ef",
+    "url": "/static/js/runtime-main.dd437d74.js"
+  }
+]);
